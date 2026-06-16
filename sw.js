8ffc8e4e42870ec448ca0d0e3bbb053ca13de5f9
JulianTannberg <julian.tannberg@gmail.com>
2026-06-16 12:11:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Chinesisch festigen – Service Worker
 // Bei jeder Änderung an der Website die Versionsnummer erhöhen,
 // damit alle Geräte die neuen Dateien laden.
-const CACHE = "cf-v48";
+const CACHE = "cf-v49";
 
 const SHELL = [
   "./",
