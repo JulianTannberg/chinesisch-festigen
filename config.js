f4725b712d17c200ded1786cbad6e59e3dfd4596
JulianTannberg <julian.tannberg@gmail.com>
2026-06-23 11:38:22 +0200

    l

--- config.js
+++
@@ -1,4 +0,0 @@
-// Öffentliche Konfiguration (KEIN API-Key hier!).
-// Nach dem Deploy des Cloudflare Workers dessen URL eintragen, z. B.:
-// window.CF_CONFIG = { workerUrl: "https://cf-transcribe.dein-name.workers.dev" };
-window.CF_CONFIG = { workerUrl: "" };
