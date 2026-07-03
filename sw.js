d9ead454cfd5f9c846d663f2dda8a39c3ff5b56b
JulianTannberg <julian.tannberg@gmail.com>
2026-07-03 21:44:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Chinesisch festigen – Service Worker
 // Bei jeder Änderung an der Website die Versionsnummer erhöhen,
 // damit alle Geräte die neuen Dateien laden.
-const CACHE = "cf-v117-le-guo-mei";
+const CACHE = "cf-v118-le-guo-mei";
 
 const SHELL = [
   "./",
